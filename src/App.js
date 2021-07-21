0c0d115bc8139fdf82c03d78f4435878e08d0c0c
krupa2029 <kjkotia10@gmail.com>
2021-07-20 21:53:58 -0700

    Feat: Added Expense Filter Filter Expense Items By Year

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
+
 const App = () => {
   //Object
   const expenses = [
     { id: 'e1', title: 'Car Insuarance', amount: 294.67, date: new Date(2021, 2, 28) },
-    { id: 'e2', title: 'Toilet Paper', amount: 24.00, date: new Date(2021, 9, 2) },
-    { id: 'e3', title: 'Book', amount: 94.60, date: new Date(2021, 4, 29) },
+    { id: 'e2', title: 'Toilet Paper', amount: 24.00, date: new Date(2020, 9, 2) },
+    { id: 'e3', title: 'Book', amount: 94.60, date: new Date(2020, 4, 29) },
     { id: 'e4', title: 'Laptop', amount: 1294.67, date: new Date(2021, 2, 19) },
   ];
 
