9f2afd2a3763921f2c576c2f632e067cc1747704
krupa2029 <kjkotia10@gmail.com>
2021-09-22 14:18:55 +0530

    Dynamic expense-list: Renderd list of user input Fix: unique 'key' error

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React from "react";
+import React,{ useState } from "react";
 
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
+const DUMMY_EXPENSES = [
+  { id: 'e1', title: 'Car Insuarance', amount: 294.67, date: new Date(2021, 2, 28) },
+  { id: 'e2', title: 'Toilet Paper', amount: 24.00, date: new Date(2020, 9, 2) },
+  { id: 'e3', title: 'Book', amount: 94.60, date: new Date(2020, 4, 29) },
+  { id: 'e4', title: 'Laptop', amount: 1294.67, date: new Date(2021, 2, 19) },
+];
 
 const App = () => {
-  //Object
-  const expenses = [
-    { id: 'e1', title: 'Car Insuarance', amount: 294.67, date: new Date(2021, 2, 28) },
-    { id: 'e2', title: 'Toilet Paper', amount: 24.00, date: new Date(2020, 9, 2) },
-    { id: 'e3', title: 'Book', amount: 94.60, date: new Date(2020, 4, 29) },
-    { id: 'e4', title: 'Laptop', amount: 1294.67, date: new Date(2021, 2, 19) },
-  ];
+  
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
     console.log(expense);
   };
+  
 
   return (
     <div>
