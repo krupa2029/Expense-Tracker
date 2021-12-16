d76e97a4052113a38c4b2d54ca57381982a57190
krupa2029 <kjkotia10@gmail.com>
2021-12-16 16:52:38 +0530

    Sending GET request to retrieve data

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
-import React,{ useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
-
-const DUMMY_EXPENSES = [
-  { id: 'e1', title: 'Car Insuarance', amount: 294.67, date: new Date(2021, 2, 28) },
-  { id: 'e2', title: 'Toilet Paper', amount: 24.00, date: new Date(2020, 9, 2) },
-  { id: 'e3', title: 'Book', amount: 94.60, date: new Date(2020, 4, 29) },
-  { id: 'e4', title: 'Laptop', amount: 1294.67, date: new Date(2021, 2, 19) },
-];
+import useHttp from "./hook/use-http";
+import { getAllExpenses } from "./lib/api";
 
 const App = () => {
-  
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const {
+    sendRequest,
+    status,
+    data: loadedExpenses,
+    error,
+  } = useHttp(getAllExpenses, true);
 
-  const addExpenseHandler = (expense) => {
-    setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses];
-    });
-    console.log(expense);
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  console.log(loadedExpenses);
+
+  const onAddExpenseHandler = (added_expense_status) => {
+    if (added_expense_status === "completed") {
+      sendRequest();
+    }
   };
-  
+  let expenses_list = <p>No Expense Found</p>;
+  // if (status === "completed" && (!loadedQuotes || loadedQuotes.length === 0)) {
+  //   return <p>No Expense Found</p>;
+  // }
+  if (status === "completed" && loadedExpenses && loadedExpenses.length > 0) {
+    expenses_list = <Expenses items={loadedExpenses} />;
+  }
 
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
+      <NewExpense onAddExpense={onAddExpenseHandler} />
+      {/* <Expenses items={expenses} /> */}
+      {expenses_list}
     </div>
-  )
-}
+  );
+};
 
 export default App;
