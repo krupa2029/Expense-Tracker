ef3f063d31e084dd0ff86fe1256e65efc6de097a
krupa2029 <kjkotia10@gmail.com>
2021-12-16 16:54:30 +0530

    Sending POST request to store a new-expense data

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,42 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import useHttp from "../../hook/use-http";
+import { addExpense } from "../../lib/api";
 
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
-    const [showForm, setShowForm] = useState(false);
+  const { sendRequest, status, error } = useHttp(addExpense);
+  const [showForm, setShowForm] = useState(false);
 
-    const saveExpenseDataHandler = (enteredExpenseData) => {
-        const expenseData = {
-            ...enteredExpenseData,
-            id: Math.random().toString()
-        };
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    sendRequest(enteredExpenseData);
 
-        props.onAddExpense(expenseData);
-        setShowForm(false);
-    };
+    props.onAddExpense(status);
+    setShowForm(false);
+  };
 
-    const showFormHandler = () => {
-        setShowForm(true);
-    };
+  const showFormHandler = () => {
+    setShowForm(true);
+  };
 
-    const hideFormHandler = () => {
-        setShowForm(false);
-    }
+  const hideFormHandler = () => {
+    setShowForm(false);
+  };
 
-
-    return (<div className="new-expense">
-
-        {!showForm &&
-            <button onClick={showFormHandler}>Add New Expense</button>
-        }
-        {showForm &&
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}
-                onCancel={hideFormHandler}
-            />
-        }
-
-    </div>);
+  return (
+    <div className="new-expense">
+      {!showForm && <button onClick={showFormHandler}>Add New Expense</button>}
+      {showForm && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={hideFormHandler}
+        />
+      )}
+    </div>
+  );
 };
 
 export default NewExpense;
